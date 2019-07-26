d9a17a62b78ae0b0b8d9c8a04752f8a947b6c3ab
Hugo Andrés Vázquez <hugo@nubeit.net>
2019-07-26 18:11:11 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const middleware = require("webpack-dev-middleware");
 const webpackOptions = require("../webpack.config.js");
 const compiler = webpack(webpackOptions);
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
